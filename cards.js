1ec90443b16af9665baf03561a71ad64b289d969
MrKaiKendal <thehighestcount@gmail.com>
2026-05-09 15:35:12 -0400

    Update cards.js

--- cards.js
+++ cards.js
@@ -71,4 +71,12 @@
     proof: "https://www.tcgplayer.com/product/listing/fqCyIZ4BlbI1",
       dateAdded: "2026-05-09"
     },
+    455: {
+    status: "Confirmed",
+    owner: "Unknown",
+    note: "Spotted in TCG on an old listing.",
+    image: "images/455.jpg",
+    proof: "https://www.tcgplayer.com/product/listing/E_mW37tl8fQ1",
+      dateAdded: "2026-05-09"
+    },
 };
